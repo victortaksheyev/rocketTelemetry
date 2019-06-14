20817668c7f3d3e853844e859f87f4dc5e4ba441
Victor Taksheyev <victortaksheyev@gmail.com>
2019-06-14 15:14:52 -0700

    Add files via upload

---
+++ final/payloadDeployment.ino
@@ -0,0 +1,172 @@
+#include <Wire.h>
+#include <Adafruit_MPL3115A2.h>
+
+
+// Connect  3.3 to 3vo
+//          gnd to gnd
+//          SCL to SCL (A5 on NANO)
+//          SDA to SDA (A4 on NANO)
+
+
+Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
+
+// global vars ---------------------------------------------------
+float initH = 0;
+float maxH = 0;
+bool firstRun = true;
+const int minDeploymentH = 850;                                   // feet
+const int maxDeploymentH = 1000;                                  // feet maybe change this to 1100
+float prevAlt = -1;
+float currAlt;
+bool noseConeDeployed = true;
+
+const int delayTime = 15000;
+const int releaseSignal = 5;
+
+int seconds = 0;
+bool initCall = false;
+const int sampleTime = 1;                                         // performing redundant check algo every second
+int callTime = 0;
+
+
+float initAlt;
+float finalAlt;
+bool enable = false;
+
+// global vars ---------------------------------------------------
+
+
+class Time {
+  public:
+    int count;
+    int seconds;
+    const int delay = 250;
+    Time();
+    void printSecs();
+    void incrementSecs();
+};
+
+Time::Time() {
+  count = 0;
+  seconds = 1;
+}
+
+void Time::incrementSecs() {
+  count+=1;
+  if (count == 2) {
+    seconds++;
+//    Serial.println(seconds);            // prints out secs
+    count = 0;
+   }
+}
+
+Time clock;
+
+float mToF(float h);
+void getInitialAlt(float& initAlt);
+void getMax(float alt, float& max);
+bool inDeployRange(float currAlt, float maxDeploymentH, float minDeploymentH);
+bool decreasingH(float currH, float prevH);
+float altChange(float initAlt, float finalAlt);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(releaseSignal, OUTPUT);
+}
+ 
+void loop() {
+  if (! baro.begin()) {                           // not an option
+    Serial.println("Couldnt find sensor");
+    return;
+  } 
+  if (firstRun) {                                    // will only run 1 time
+    getInitialAlt(initH);  
+    firstRun = false;
+    prevAlt = -1;
+  } else {
+    prevAlt = currAlt;  
+  }
+  
+  currAlt = mToF(baro.getAltitude()) - initH;
+
+  getMax(currAlt, maxH);
+
+//  ------------------------------------------------------------------ MAIN SYSTEM ---------------------------------------------------------------------
+  if ( decreasingH(currAlt, prevAlt) && inDeployRange(currAlt, maxDeploymentH, minDeploymentH) && currAlt < maxH) {
+    Serial.println("------------- DEPLOYED (via main system) ----------------");
+    digitalWrite(releaseSignal, HIGH);    // REPLACE THIS WITH TRANSISTOR CODE
+    delay(delayTime);                // send current for 5 seconds
+    while(1);                   // BOOM we good
+  }
+
+// ------------------------------------------------------------ END MAIN SYSTEM ---------------------------------------------------------------------
+
+
+
+//  ------------------------------------------------------------------ REDUNDANT SYSTEM ---------------------------------------------------------------------
+  if (clock.seconds % sampleTime == 0 && initCall == false) {
+    callTime = clock.seconds;
+    initAlt = currAlt;
+    initCall = true;                                      // declaring that initial height has been sampled so it doesnt repeat
+    Serial.print("Initial alt sampled - ");Serial.println(initAlt);
+    }
+
+
+  if (clock.seconds == (callTime + sampleTime) && initCall == true) {
+    finalAlt = currAlt;
+    Serial.print("Final alt sampled - "); Serial.println(finalAlt);
+    
+    
+    Serial.print("Change in altitude: "); Serial.println(altChange(initAlt, finalAlt));
+    
+    
+    if (altChange(initAlt, finalAlt) > 50) {  // verifies that the rocket has TAKEN OFF (has been in the air)
+      enable = true;
+    }
+
+    if ( currAlt < minDeploymentH && currAlt < maxH && enable && decreasingH(currAlt, prevAlt)) {
+      digitalWrite(releaseSignal, HIGH); // REPLACE THIS WITH TRANSISTOR SIGNAL
+      delay(delayTime);                                                                            // for 5 seconds
+      Serial.println("------------- DEPLOYED (via redundant system) ----------------");
+      while(1);                                
+    }
+    initCall = false;
+  }
+    delay(250);
+    clock.incrementSecs();                     // checks and increments count, increments seconds after 4 incremenets of count
+}
+
+//  ---------------------------------------------------------- END REDUNDANT SYSTEM ---------------------------------------------------------------------
+void getInitialAlt(float& initAlt) {
+  float A;
+  for (int i = 0; i < 5; i++) {                // sample the altitude 5 times to confirm it's stable
+    A = baro.getAltitude();
+  }
+  initAlt = mToF(A);                           // return the altitude in feet
+}
+
+float mToF(float h){          
+  return (h * 3.281);
+}
+
+void getMax(float alt, float& maximumH){
+  if (alt > maximumH) maximumH = alt;
+}
+
+bool inDeployRange(float currAlt, float maxDeploymentH, float minDeploymentH){
+  if (currAlt <= maxDeploymentH && currAlt >= minDeploymentH) {
+    return true;  
+  }
+  return false;
+}
+
+bool decreasingH(float currH, float prevH) {
+  if (currH < prevH) return true;
+  else return false;
+}
+
+// calculates and returns difference between initial and final altitudes
+float altChange(float initAlt, float finalAlt) {
+    return abs(finalAlt-initAlt);
+}
+
---
+++ final/telemetry.ino
@@ -0,0 +1,193 @@
+#include <SparkFunMPU9250-DMP.h>
+#include "SparkFunBME280.h"
+#include <Wire.h> //allows you to communicate with I2C devices
+
+#define serial SerialUSB
+#include <Servo.h> // servo
+
+// creates internal clock
+class Time {
+  public:
+    int count;
+    int seconds;
+    const int delay = 100;  // must be a number < 100
+    Time();
+    void printSecs();
+    void incrementSecs();
+};
+
+Time::Time() {
+  count = 0;
+  seconds = 1;
+}
+
+void Time::incrementSecs() {
+  count+=1;
+  if (count == 10) {
+    seconds++;
+    serial.println(seconds);            // prints out secs
+    count = 0;
+   }
+}
+
+int callTime = 0;                       // stores time when alt is sampled
+float initAlt;                          // stores init alt sample of interval
+float finalAlt;                         // stores final alt sample of interval
+
+bool initCall = false;                  // stores if alt is initially sampled
+bool enable = false;                    // enables rocket to inflate payload (will only occur if Δalt > certain const)
+                                        
+bool inflated = false;                  // stores inflation status of payload
+                                          
+MPU9250_DMP imu;                        // creates imu sensor object
+BME280 mySensor;                        // creates altimeter sensor object
+Servo myservo;                          // creates servo object
+Time mainClock;                         // creates main clock/time that rocket runs on
+
+const int lockInput = 10;               // second armed mechanism
+const int servo_pin = 11;               // output to the servo
+const int start_angle = 0;
+const int sampleTime = 1;
+
+bool firstRun = true;
+bool lockEnable = false;
+
+double initHeight;
+
+// configuring the sensor and pins
+void setup() {
+  myservo.attach(servo_pin);
+  myservo.write(start_angle);
+  Serial.begin(115200);
+  Wire.begin();
+
+  
+ if (mySensor.beginI2C() == false) {    // Begin communication over I2C 
+    Serial.println("The sensor did not respond. Please check wiring.");
+    while(1); //Freeze
+  }
+  if (imu.begin() != INV_SUCCESS) {
+    while (1) {
+      serial.println("Unable to communicate with MPU-9250");
+      serial.println("Check connections, and try again.");
+      serial.println();
+      delay(5000);
+    }
+  }
+  
+  imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);
+  // Gyro options are +/- 250, 500, 1000, or 2000 dps
+  imu.setGyroFSR(2000);   // Set gyro to 2000 dps
+  imu.setAccelFSR(16);    // Set accel to full range
+
+  imu.setLPF(5);          // Set LPF corner frequency to 5Hz
+  imu.setSampleRate(10);  // Set sample rate to 10Hz
+
+  // sampling initial height
+  
+
+  
+}
+
+// ----------------------------------------------------------- END OF SETUP -----------------------------------------------------------
+
+// ----------------------------------------------------------- START OF LOOP ----------------------------------------------------------
+
+void loop() {
+
+  if (lockEnable) {
+    
+    if (firstRun) {
+    // samples initial height 5 times
+    for (int i = 0; i < 5; i++){
+         initHeight = mySensor.readFloatAltitudeFeet();
+    }
+    firstRun = false;   // prevent it from running again
+  }
+
+//  serial.print("initial Altitude"); serial.println(initHeight);
+  double alt;
+  double accelz;
+  double accely;
+  double gyroz;
+  
+  if ( imu.dataReady() ) {
+    imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
+  }
+
+  if (mainClock.seconds % sampleTime == 0 && initCall == false) {
+    callTime = mainClock.seconds;
+    initAlt = mySensor.readFloatAltitudeFeet();           // samples alt, storing it as initial alt
+//    serial.print("INITIAL height = "); serial.println(initAlt);
+    initCall = true;                                      // declaring that initial height has been sampled so it doesnt repeat
+  }
+
+  if (mainClock.seconds == (callTime + sampleTime) && initCall == true) {
+    finalAlt = mySensor.readFloatAltitudeFeet();          // samples alt, storing it as final alt
+    serial.print("FINAL height = "); serial.println(finalAlt);
+    serial.print("Change in altitude: "); serial.println(altChange(initAlt, finalAlt));
+    
+    if (altChange(initAlt, finalAlt) > 50) {                  // verifies that the rocket has taken off (has been in the air)
+      enable = true;
+    }
+    
+    int totalAngle = 0;
+    if (altChange(initAlt, finalAlt) < 2 && enable && (finalAlt < (initHeight + 300))) {         // open valve
+      for(int angle = 0; angle <= 350; angle += 5) {                 
+          myservo.write(angle);                     
+          delay(50);
+          totalAngle += 5;
+          inflated = true;               
+        }
+        delay(7500);    // wait 7.5 seconds
+
+       // close valve
+       for (int angle = totalAngle; angle >= 0; angle -= 5) {
+          myservo.write(angle);
+          delay(50);
+       }
+        serial.println("------------------ THE VALAVE IS CLOSED--------------------------------");                                       
+        stop();                                               // end 
+    }
+    
+    initCall = false;
+   }
+
+   
+   mainClock.incrementSecs();
+   delay(mainClock.delay);    // uses the dalay preset in the time
+    
+  } else {
+    // check if the lock has been enabled
+    if (digitalRead(lockInput) == HIGH) {
+      lockEnable = true;  
+    }  
+  
+  }
+
+  
+    
+}
+
+// ----------------------------------------------------------- END OF LOOP ----------------------------------------------------------
+
+// ----------------------------------------------------------- FUNCTION DEFs --------------------------------------------------------
+
+// initializes main rocket clock, samples and prints sensor data, every DELAY amount
+
+// calculates and returns difference between initial and final altitudes
+float altChange(float initAlt, float finalAlt) {
+    return abs(finalAlt-initAlt);
+}
+
+// acts as a permanent end to the program
+// only call when program needs to be ended entirely
+void stop()
+{
+ while(1);                  // calls an infinite loop
+}
+
+
+// ----------------------------------------------------------- END FUNCTION DEFs -----------------------------------------------------
+
+
